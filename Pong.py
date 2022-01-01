cbb736f7cfdabc7313fca56b403743a60763f5b8
Elisa Pan Huang <80305459+Epanhua622@users.noreply.github.com>
2022-01-01 13:43:40 -0800

    commit on main

--- Pong.py
+++ Pong.py
@@ -1,9 +1,10 @@
 import turtle
+import os
 
 wn = turtle.Screen()
 wn.title("Pong by @Epanhua622")
 wn.bgcolor("black")
-wn.setup(width=800,height=600)
+wn.setup(width=800, height=600)
 wn.tracer(0)
 
 # Paddle A
@@ -13,7 +14,7 @@ paddle_a.shape("square")
 paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5,stretch_len=1)
 paddle_a.penup()
-paddle_a.goto(-350,0)
+paddle_a.goto(-350, 0)
 
 # Paddle B
 paddle_b = turtle.Turtle()
@@ -22,15 +23,17 @@ paddle_b.shape("square")
 paddle_b.color("white")
 paddle_b.shapesize(stretch_wid=5,stretch_len=1)
 paddle_b.penup()
-paddle_b.goto(350,0)
+paddle_b.goto(350, 0)
 
 # Ball
 ball = turtle.Turtle()
 ball.speed(0)
-ball.shape("circle")
+ball.shape("square")
 ball.color("white")
 ball.penup()
-ball.goto(0,0)
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
 
 # Functions
 def paddle_a_up():
@@ -38,9 +41,28 @@ def paddle_a_up():
     y += 20
     paddle_a.sety(y)
 
-# Keyboard binding
-    wn.listen()
-    wn.onkeypress(paddle_a_up, "Up")
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
 
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+# Main game loop
 while True:
     wn.update()
